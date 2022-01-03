70c7db0d9f002bedba81adbd3fb7de43c11bfdc7
StckOverflw <stckoverflw@gmail.com>
2022-01-03 16:28:21 +0100

    Add Contact Page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,106 @@
 <template>
-  <div class="vertical-center contact">
-    <h1>Coming soon&trade;</h1>
+  <div class="contact">
+    <h1 class="subpage-title animate">Contact Me</h1>
+    <div class="subtitle animate delay02">
+      <p>
+        Let's make something awesome!
+      </p>
+    </div>
+    <div class="subpage-text animate delay04">
+      <p>
+        I love to collaborate with minecraft content creators and help to create the best content possible!
+      </p>
+    </div>
+    <div class="subpage-text animate delay08">
+      <p>
+        If you think you have something awesome in mind feel free to contact me one of the platforms below.
+      </p>
+    </div>
+    <div class="links animate delay1">
+      <div class="link">
+        <a class="on-hover text-color-secondary label-email copy-email"></a>
+      </div>
+      <div class="link">
+        <a class="on-hover text-color-secondary link label-twitter" href="https://twitter.com/StckOverflw" target="_blank" rel="noopener noreferrer"></a>
+      </div>
+      <div class="link">
+        <a class="on-hover text-color-secondary link label-discord" href="discord://-/users/816989010836717599" target="_blank" rel="noopener noreferrer"></a>
+      </div>
+    </div>
+    <div class="subpage-text animate delay12">
+      <p>
+        You can also follow me on these other social media platforms:
+      </p>
+    </div>
+    <div class="links animate delay14">
+      <div class="link">
+        <a class="on-hover text-color-secondary link" href="https://github.com/StckOverflw" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </div>
+      <div class="link">
+        <a class="on-hover text-color-secondary link" href="https://twitch.tv/StckOverflw" target="_blank" rel="noopener noreferrer">Twitch</a>
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import {Vue} from "vue-class-component";
+import ClipboardJS from "clipboard";
+
+const clipboard = new ClipboardJS('.copy-email', {
+  text: function () {
+    return 'stckoverflw@gmail.com';
+  }
+});
+
+clipboard.on('success', function(e) {
+  console.log(e);
+});
+clipboard.on('error', function(e) {
+  console.log(e);
+});
+
+export default class Home extends Vue {
+
+  copy(text: string): void {
+    new ClipboardJS('.btn');
+    navigator.clipboard.writeText(text);
+  }
+}
+</script>
+
+<style scoped>
+  .label-email:before {
+    content: "stckoverflw@gmail.com";
+  }
+  .label-email:hover:before {
+    content: "Click to copy to clipboard";
+  }
+  .label-twitter:before {
+    content: "Twitter";
+  }
+  .label-twitter:hover:before {
+    content: "StckOverflw";
+  }
+  .label-discord:before {
+    content: "Discord";
+  }
+  .label-discord:hover:before {
+    content: "StckOverflw#2665";
+  }
+  .links {
+    font-size: 1.5em;
+    font-weight: bolder;
+    margin-left: 10vmin;
+  }
+  .link {
+    margin-bottom: 2vh;
+  }
+  .on-hover {
+    transition: all .4s cubic-bezier(0.2, 0.6, 0.2, 1);
+  }
+  .on-hover:hover {
+    margin-left: 20px;
+    transition: all .4s cubic-bezier(0.2, 0.6, 0.2, 1);
+  }
+</style>
